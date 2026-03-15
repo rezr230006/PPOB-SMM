bb3e1d512ac32349055358a6f390297ac9909f2a
rezr230006 <rezr230006@gmail.com>
2026-03-15 17:59:03 +0700

    Added WebhookHandler.php for handling payment and SMM webhooks

---
+++ gateway/WebhookHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Gateway;
+
+class WebhookHandler
+{
+    public function handlePaymentWebhook($data) {
+        // Handle payment webhook logic here
+    }
+
+    public function handleSMMWebhook($data) {
+        // Handle SMM webhook logic here
+    }
+}
